8e9d05cf6357ccb005df5de661da0306d11a4b33
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-11-02 00:05:05 +0300

    changed homepage

--- src/api/contactFormApi.ts
+++ src/api/contactFormApi.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'https://node-js-gmail-eight.vercel.app/',
+    baseURL: 'node-js-gmail-ivory.vercel.app',
 })
 
 export const contactFormApi = {
     sendFormData(data: ContactFormValue) {
-        return instance.post("sendMessage", data)
+        return instance.post("/sendMessage", data)
     },
 }
 
