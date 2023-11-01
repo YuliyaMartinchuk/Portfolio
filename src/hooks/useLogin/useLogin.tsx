2484816247e718e23109b8832e64dc3f75ed7303
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-11-02 00:21:19 +0300

    added logic in validate

--- src/hooks/useLogin/useLogin.tsx
+++ src/hooks/useLogin/useLogin.tsx
@@ -17,12 +17,11 @@ export const useLogin = () => {
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
                 errors.email = 'Invalid email address';
             }
-
-            // if (!values.name) {
-            //     errors.name = 'Required';
-            // } else if (values.name.length < 2) {
-            //     errors.name = 'Please add more symbols';
-            // }
+            if (!values.name) {
+                errors.name = 'Required';
+            } else if (values.name.length < 2) {
+                errors.name = 'Please add more symbols';
+            }
             if (!values.message) {
                 errors.message = 'Required';
             }
