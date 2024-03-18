5ed845494e1741d9e2f1b70803e3831edec8fbe2
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2024-03-18 23:01:46 +0200

    added animation for Project

--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,6 +1,9 @@
 import React, {FC} from 'react';
-import { ProjectContainer} from "./ProjectStyle";
-
+import {ProjectContainer} from "./ProjectStyle";
+import { motion } from "framer-motion";
+import {
+    primaryColor
+} from "../../common/styles/variables"
 export type ProjectType = {
     image: string
     title: string
@@ -9,20 +12,36 @@ export type ProjectType = {
     gitHubHref: string
 }
 
-export const Project: FC<ProjectType> = ({image,title,description,href, gitHubHref}) => {
+export const Project: FC<ProjectType> = ({
+                                             image,
+                                             title,
+                                             description,
+                                             href,
+                                             gitHubHref
+                                         }) => {
     return (
-    <ProjectContainer id={"ProjectContainer"} >
-        <div id={"ImageContainer"}>
-            <img  src={image}/>
-            <a href={href} target="_blank" className="project-link" >See project</a>
-        </div>
-        <div id={"TextContainer"}>
-            <a href={href} target="_blank">
-                <h3>{title}</h3>
-            </a>
-            <p>{description}</p>
-        </div>
-    </ProjectContainer>
+        <motion.div
+            tabIndex={0}
+            whileHover={{
+                scale: 1.005,
+                transition: { duration: 0.15 },
+                boxShadow: `0 0px 12px ${primaryColor}`,
+            }}
+        >
+        <ProjectContainer id={"ProjectContainer"}>
+            <div id={"ImageContainer"}>
+                <img src={image}/>
+                <a href={href} target="_blank" className="project-link">See
+                    project</a>
+            </div>
+            <div id={"TextContainer"}>
+                <a href={href} target="_blank">
+                    <h3>{title}</h3>
+                </a>
+                <p>{description}</p>
+            </div>
+        </ProjectContainer>
+        </motion.div>
     );
 };
 
