30a4c307343edccc8572a02d6f1037891874a554
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2024-03-19 23:54:54 +0300

    added animation for Skill

--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,7 +1,6 @@
 import React, {FC} from 'react';
 import {SkillContainer} from './SkillStyle';
 
-
 export type SkillType = {
     image: string
     title: string
@@ -12,6 +11,7 @@ export const Skill: FC<SkillType> = ({image,title}) => {
         <SkillContainer id={"SkillContainer"}>
             <img src={image} />
             <p>{title}</p>
+            <div></div>
         </SkillContainer>
     );
 };
--- src/skills/skill/SkillStyle.ts
+++ src/skills/skill/SkillStyle.ts
@@ -1,15 +1,18 @@
 import styled from "styled-components";
-import {borderColor, primaryColor} from "../../common/styles/variables"
+import {borderColor, primaryColor, primaryTransparentColor} from "../../common/styles/variables"
 
 export const SkillContainer = styled.div`
-  width: 130px;
+  position: relative;
+  width: 140px;
+  height: 140px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   border: 1px solid ${borderColor};
   img {
-    margin-top: 2%;
+   justify-content: center;
+    margin-top: 6%;
     height: 60px;
     width: 60px;
     background-color: rgba(0, 0, 0, 0);
@@ -25,14 +28,49 @@ export const SkillContainer = styled.div`
     width: 100%;
     font-family: Poppins, sans-serif;
     word-break: break-word;
+    white-space: pre-wrap;
 
     &:hover {
       filter: brightness(0.7);
     }
   }
+  div {
+    position: absolute;
+    left: -2.5px;
+    top: -52.5px;
+    width: 10px;
+    height: 10px;
+    background-color: ${primaryTransparentColor};
+    animation: move 5s linear infinite;
+    offset-path: path("M 50,50 m -50,0 a 50,50 0 1,0 100,0 a 50,50 0 1,0 -100,0");
+    animation-play-state: paused;
+
+  }
+  &:hover div {
+    animation-play-state: running;
+  }
+
+  @keyframes move {
+    0% {
+      transform: translate(0, 0);
+    }
+    25% {
+      transform: translate(137.5px, 0);
+    }
+    50% {
+      transform: translate(137.5px, -137.5px);
+    }
+    75% {
+      transform: translate(0,-137.5px);
+    }
+    100% {
+      transform: translate(0, 0);
+    }
 
   @media (max-width: 250px) {
     width: 100%;
+    height: auto;
+  }
+
   }
 `
-
